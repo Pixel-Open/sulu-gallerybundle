450b696fc2188b8f1d61bbf538becbd67e2a1ddc
Matthieu <support@pixel-developpement.com>
2022-03-09 11:13:01 +0100

    gitflow-release-stash: 2.4.0

---
+++ Entity/AlbumTranslation.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Pixel\GalleryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Sulu\Component\Persistence\Model\AuditableInterface;
+use Sulu\Component\Persistence\Model\AuditableTrait;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="gallery_album_translation")
+ * @ORM\Entity(repositoryClass="Pixel\GalleryBundle\Repository\AlbumRepository")
+ * @Serializer\ExclusionPolicy("all")
+ */
+class AlbumTranslation implements AuditableInterface
+{
+    use AuditableTrait;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     *
+     * @Serializer\Expose()
+     */
+    private ?int $id = null;
+
+    /**
+     * @var Album
+     *
+     * @ORM\ManyToOne(targetEntity="Pixel\GalleryBundle\Entity\Album", inversedBy="translations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $album;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=5)
+     */
+    private $locale;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose()
+     */
+    private string $name;
+
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     *
+     * @Serializer\Expose()
+     */
+    private ?string $description;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose()
+     */
+    private string $routePath;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     *
+     * @Serializer\Expose()
+     */
+    private ?array $seo = null;
+
+
+    public function __construct(Album $album, string $locale)
+    {
+        $this->album = $album;
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAlbum(): Album
+    {
+        return $this->album;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+    public function getRoutePath(): string
+    {
+        return $this->routePath ?? '';
+    }
+
+    public function setRoutePath(string $routePath): void
+    {
+        $this->routePath = $routePath;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getSeo(): ?array
+    {
+        return $this->seo;
+    }
+
+    /**
+     * @param array|null $seo
+     */
+    public function setSeo(?array $seo): void
+    {
+        $this->seo = $seo;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+}
---
+++ Sitemap/GallerySitemapProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pixel\GalleryBundle\Sitemap;
+
+use Pixel\GalleryBundle\Repository\AlbumRepository;
+use Sulu\Bundle\WebsiteBundle\Sitemap\Sitemap;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapProviderInterface;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapUrl;
+use Sulu\Component\Webspace\Manager\WebspaceManagerInterface;
+
+class GallerySitemapProvider implements SitemapProviderInterface
+{
+    /**
+     * @var AlbumRepository
+     */
+    private AlbumRepository $albumRepository;
+    private WebspaceManagerInterface $webspaceManager;
+    private array $locales = [];
+
+    /**
+     * @param AlbumRepository $albumRepository
+     */
+    public function __construct(AlbumRepository $albumRepository, WebspaceManagerInterface $webspaceManager)
+    {
+        $this->albumRepository = $albumRepository;
+        $this->webspaceManager = $webspaceManager;
+    }
+
+    public function build($page, $scheme, $host): array
+    {
+        $locale = $this->getLocaleByHost($host);
+        $result = [];
+        foreach ($this->albumRepository->findAllForSitemap((int)$page, (int)self::PAGE_SIZE) as $album) {
+            //$album->setLocale($locale);
+            $result[] = new SitemapUrl(
+                $scheme . '://' . $host . $album->getRoutePath(),
+                $album->getLocale(),
+                $album->getLocale(),
+                new \DateTime()
+            );
+        }
+
+        return $result;
+    }
+
+    private function getLocaleByHost($host)
+    {
+        if (!\array_key_exists($host, $this->locales)) {
+            $portalInformation = $this->webspaceManager->getPortalInformations();
+            foreach ($portalInformation as $hostName => $portal) {
+                if ($hostName === $host) {
+                    $this->locales[$host] = $portal->getLocale();
+                }
+            }
+        }
+        if (isset($this->locales[$host])) return $this->locales[$host];
+    }
+
+    public function createSitemap($scheme, $host): Sitemap
+    {
+        return new Sitemap($this->getAlias(), $this->getMaxPage($scheme, $host));
+    }
+
+    public function getAlias(): string
+    {
+        return 'gallery';
+    }
+
+    public function getMaxPage($scheme, $host)
+    {
+        return ceil($this->albumRepository->countForSitemap() / self::PAGE_SIZE);
+    }
+}
